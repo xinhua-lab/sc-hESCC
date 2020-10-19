8d6bc510bb9a474b4d238978602178e1d0502b88
ZheyiChen <58262162+ZheyiChen@users.noreply.github.com>
2020-10-20 01:46:52 +0800

    Update trajectory_inference.R

--- trajectory_inference.R
+++ trajectory_inference.R
@@ -5,50 +5,50 @@ library(Matrix)
 library(umap)
 library(dplyr)
 library('scran')
-library(RColorBrewer)
-library(Rtsne)
+library(readxl)
+library(monocle)
+library(xlsx)
 rm(list=ls())
 graphics.off()
 #memory.limit(size=56000)
 
 setwd(workpath)
+#fig 5c
 #load data
-cds_for_pseudo <- readRDS(file = "seuratobjdata_T_traj.rds") # 10000 random picked cells
+seuratobj.data <- readRDS("processed.rds")
+cluster <- seuratobj.data$seurat_clusters
+# Select the clusters want to map
+idx_sel <- c(which(cluster==1), which(cluster==2), which(cluster==3)
+             )
+
+cluster <- cluster[idx_sel]
+
+expression <- as.matrix(seuratobj.data@assays$integrated@data[,idx_sel])
+gene_feature <- rownames(expression)
+gene_feature <- as.matrix(gene_feature)
+rownames(gene_feature) <- gene_feature
+colnames(gene_feature) <- "gene_short_name"
+gene_feature <- data.frame(gene_feature)
+cluster <- data.frame(cluster)
+pd <- new("AnnotatedDataFrame", data = cluster);
+fd <- new("AnnotatedDataFrame", data = gene_feature);
+cds <- newCellDataSet(expression, phenoData = pd, featureData = fd);
+
+cds<- estimateSizeFactors(cds);
+cds <- estimateDispersions(cds);
+
+disp_table <- dispersionTable(cds);
+ordering_genes <- subset(disp_table, mean_expression >= 0.1);
+cds <- setOrderingFilter(cds, ordering_genes$gene_id)
+
+#cds_reduced <- reduceDimension(cds);
 
 
-tiff('NAME.tiff', units="in", width=5, height=4, res=300, compression = 'lzw')
-plot_cell_trajectory(cds_for_pseudo , markers_linear = TRUE, color_by = "cluster", cell_size = 0.5) + scale_color_brewer(palette="Dark2")
-dev.off()
+cds_reduced <- reduceDimension(cds)#, max_components = 2, num_dim = 6, reduction_method = 'DDRTree', verbose = T)
 
-DefaultAssay(seurat.objdata) <- "RNA"
-seuratobjdata_T <- seurat.objdata[,idx_T]
+cds_for_pseudo <- orderCells(cds_reduced)
 
 
-#T <- read_excel("T signatures.xlsx", col_names = FALSE, sheet = 3) # self-defined gene list
-T <- read_excel("function gene set.xlsx", col_names = FALSE) # published gene list
-geneList <- T$...1
-idx_function <- match(geneList, rownames(seurat.objdata))
-idx_function <- idx_Cytotoxic[ !is.na(idx_function) ]
-
-expression <- as.matrix(expression <- as.matrix(seurat.objdata_T@assays$RNA@data[idx_function,])
-                        function_scores <- colMeans(expression, na.rm = FALSE)@assays$RNA@data[idx_function,])
-function_scores <- colMeans(expression, na.rm = FALSE)
-expr <- rbind(several function_scores)
-rownames(expr) <- c(function_name)
-
-sd <- colSds(t(expr))*sqrt((dim(expr)[2]-1)/(dim(expr)[2]))
-mean <- colMeans(t(expr), na.rm = FALSE, dims = 1)
-z_score <- matrix(0, dim(expr)[1], dim(expr)[2])
-for (i in 1:2){
-  z_score[i,] <- (expr[i,]-mean[i])/sd[i]
-}
-rownames(z_score) <- rownames(expr)
-colnames(z_score) <- colnames(expr)
-
-temp <- rbind(seuratobjdata_T@assays$RNA@data, z_score)
-seuratobjdata_T@assays$RNA@data <- temp
-markers.to.plot <- c(function_name)
-
-tiff('NAME.tiff', units="in", width=6, height=5, res=300, compression = 'lzw')
-DotPlot(function_name, features = rev(markers.to.plot), cols = c("blue", "red"), dot.scale = 16) + RotatedAxis() +  ggtitle("CD4+")
+tiff('Fig5c.tiff', units="in", width=6, height=4, res=300, compression = 'lzw')
+plot_cell_trajectory(cds_for_pseudo , markers_linear = TRUE, cell_size = 0.5, color_by = "cluster") + scale_color_brewer(palette="Dark2")
 dev.off()
